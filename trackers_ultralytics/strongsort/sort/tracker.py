fb13269ddc0c8af8f3118f56862473e06d6a4879
mathias <mathiasbensimhon@gmail.com>
2024-05-12 16:58:38 +0300

    working on the tracker

--- trackers_ultralytics/strongsort/sort/tracker.py
+++ trackers_ultralytics/strongsort/sort/tracker.py
@@ -8,6 +8,9 @@ import numpy as np
 
 from boxmot.motion.cmc import get_cmc_method
 from boxmot.utils.matching import chi2inv95
+from boxmot.trackers.strongsort.sort import iou_matching, linear_assignment
+from boxmot.trackers.strongsort.sort.track import Track
+
 
 class Tracker:
     """
@@ -63,7 +66,7 @@ class Tracker:
             track.mark_missed()
             
     
-    def update(self):
+    def update(self, detections):
         """
         Perform measurement update and track management
         
@@ -72,4 +75,95 @@ class Tracker:
                 A list of detections at the current time step
         """
         
+        # Run matching cascade
+        matches, unmatched_tracks, unmatched_detections = self._match(detections)
         
+        # Update track set
+        for track_idx, detection_idx in matches:
+            self.tracks[track_idx].update(detections[detection_idx])
+            
+        for track_idx in unmatched_tracks:
+            self.tracks[track_idx].mark_missed()
+            
+        for detection_idx in unmatched_detections:
+            self._initiate_track(detections[detection_idx])
+        
+        self.tracks = [t for t in self.tracks if not t.is_deleted()]
+        
+        # update distance metric
+        active_targets = [t.id for t in self.tracks if t.is_confirmed()]
+        features, targets = [] , []
+        for track in self.tracks:
+            if not track.is_confirmed():
+                continue
+            features += track.features
+            targets += [track.id for _ in track.features]
+        self.metric.partial_fit(
+            np.asarray(features), np.asarray(targets), active_targets
+        )
+            
+            
+    def _match(self, detections):
+        def gated_metric(tracks, dets, track_indices, detection_indices):
+            features = np.array([dets[i].feat for i in detection_indices])
+            targets = np.array([tracks[i].id for i in track_indices])
+            cost_matrix = self.metric.distance(features, targets)
+            cost_matrix = linear_assignment.gate_cost_matrix(
+                cost_matrix,
+                tracks,
+                dets,
+                track_indices,
+                detection_indices,
+                self.mc_lambda
+            )
+            
+            return cost_matrix
+        
+        # Split track set into confirmed and unconfirmed tracks
+        confirmed_tracks = [i for i, t in enumerate(self.tracks) if t.is_confirmed()]
+        unconfirmed_tracks = [i for i, t in enumerate(self.tracks) if not t.is_confirmed()]
+        
+        # Associate confirmed tracks using appearance features
+        matches_a, unmatched_tracks_a, unmatched_detections = linear_assignment.matching_cascade(
+            gated_metric,
+            self.metric.matching_threshold,
+            self.max_age,
+            self.tracks,
+            detections,
+            confirmed_tracks
+        )
+        
+        # Associate remaining tracks together with unconfirmed tracks using IOU
+        iou_track_candidates = unconfirmed_tracks + [
+            k for k in unmatched_tracks_a if self.tracks[k].time_since_update == 1
+        ]
+        
+        unmatched_tracks_a = [
+            k for k in unmatched_tracks_a if self.tracks[k].time_since_update != 1
+        ]
+        
+        matches_b, unmatched_tracks_b, unmatched_detections = linear_assignment.min_cost_matching(
+            iou_matching.iou_cost,
+            self.max_iou_dist,
+            self.tracks,
+            detections,
+            iou_track_candidates,
+            unmatched_detections
+        )
+        
+        matches = matches_a + matches_b
+        unmatched_tracks = list(set(unmatched_tracks_a + unmatched_tracks_b))
+        return matches, unmatched_tracks, unmatched_detections
+    
+    
+    def _initiate_track(self, detection):
+        self.tracks.append(
+            Track(
+                detection,
+                self._next_id,
+                self.n_init,
+                self.max_age,
+                self.ema_alpha
+            )
+        )
+        self._next_id += 1
